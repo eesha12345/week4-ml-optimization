2456a593af7d6d20b6d582c9e7c07930a3fd69ce
eesha12345 <eesha24mansoor@gmail.com>
2026-07-15 02:25:27 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import pandas as pd
 import joblib
 
 # Load model and scaler
-model = joblib.load("models/best_model.pkl")
-scaler = joblib.load("models/scaler.pkl")
+model = joblib.load("best_model.pkl")
+scaler = joblib.load("scaler.pkl")
 
 # Page Settings
 st.set_page_config(
